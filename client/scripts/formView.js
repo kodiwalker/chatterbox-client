182e2277bc7db6d06505643bc8e3639a934eabdb
Kodi <kodi.w13@gmail.com>
2023-04-07 18:54:10 -0600

    Pseudocode

--- client/scripts/formView.js
+++ client/scripts/formView.js
@@ -6,21 +6,21 @@ var FormView = {
 
   $form: $('form'),
 
-  initialize: function() {
+  initialize: function () {
     FormView.$form.on('submit', FormView.handleSubmit);
   },
 
-  handleSubmit: function(event) {
+  handleSubmit: function (event) {
     // Stop the browser from submitting the form
     event.preventDefault();
 
     // TODO: Currently, this is all handleSubmit does.
     // Make this function actually send a message to the Parse API.
-  
+
     console.log('click!');
   },
 
-  setStatus: function(active) {
+  setStatus: function (active) {
     var status = active ? 'true' : null;
     FormView.$form.find('input[type=submit]').attr('disabled', status);
   }
--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -5,20 +5,23 @@ var MessagesView = {
 
   $chats: $('#chats'),
 
-  initialize: function() {
+  initialize: function () {
     // TODO: Perform any work which needs to be done
     // when this view loads.
+    //^ call render?
   },
 
-  render: function() {
-    // TODO: Render _all_ the messages.
+  render: function () {
+    // TODO: Render _all_ the messages/ feed
+    //^ iterates over messages data strucure, calls on renderMessage for each, appends to body
   },
 
-  renderMessage: function(message) {
+  renderMessage: function (message) {
     // TODO: Render a single message.
+    //^ call render method of messageView.js and put the message in a feed div.
   },
 
-  handleClick: function(event) {
+  handleClick: function (event) {
     // TODO: handle a user clicking on a message
     // (this should add the sender to the user's friend list).
   }
